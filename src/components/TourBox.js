0526f075238da5236bec0bd1920674d152888696
AmrMustafa13 <amrmostafa1311@gmail.com>
2022-02-20 11:45:42 +0200

    tour-card-modified

--- src/components/TourBox.js
+++ src/components/TourBox.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const TourBox = ({ id }) => {
     return (
         <div className={classes['tour-box']}>
-            <h2>ID : {id}</h2>
+            <img src='https://media.istockphoto.com/photos/mosque-and-pyramids-picture-id1174818077?k=20&m=1174818077&s=612x612&w=0&h=vAutxzWDTokCJkf6010sguiHP6yc8Nzt8qePG9DEew0=' alt='country-image' />
+            <h2>Egypt</h2>
             <Link to={`/home/${id}`}><button>Book Now</button></Link>
         </div>
     );
